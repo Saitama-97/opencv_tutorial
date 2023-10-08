659346deb3168924e2dbb489736ee4931ac61088
Saitama <2822863494@qq.com>
2023-10-08 10:11:35 +0800

    图像梯度 - Sobel、Scharr 算子补充

--- code/imageGradient_C9/scharr1.py
+++ code/imageGradient_C9/scharr1.py
@@ -14,3 +14,16 @@ import numpy as np
 
 img = cv.imread("../../data/scharr.bmp")
 
+scharrx = cv.Scharr(img, cv.CV_64F, 1, 0)
+scharry = cv.Scharr(img, cv.CV_64F, 0, 1)
+
+scharrx = cv.convertScaleAbs(scharrx)
+scharry = cv.convertScaleAbs(scharry)
+
+scarrxy = cv.addWeighted(scharrx, 0.5, scharry, 0.5, 0)
+
+cv.imshow("origin", img)
+cv.imshow("scarrxy", scarrxy)
+
+cv.waitKey()
+cv.destroyAllWindows()
