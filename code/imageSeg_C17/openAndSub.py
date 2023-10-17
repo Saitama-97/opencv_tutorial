8ac446612b360aeb6c128e8db1c3c0037b9049ac
Saitama <2822863494@qq.com>
2023-10-17 17:23:57 +0800

    图像分割 - 分水岭算法（今天划水了）

---
+++ code/imageSeg_C17/openAndSub.py
@@ -0,0 +1,36 @@
+# _*_ coding: utf-8 _*_
+
+"""
+  @Time    : 2023.10.17 14:50
+  @File    : openAndSub.py
+  @Project : opencv_tutorial
+  @Author  : Saitama
+  @IDE     : PyCharm
+  @Desc    : 开运算 + 减法运算，获取图像的边界
+"""
+
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+
+o = cv.imread("../../data/my.bmp")
+
+k = np.ones((5, 5), np.uint8)
+
+e = cv.erode(o, k)
+
+b = cv.subtract(o, e)
+
+plt.subplot(131)
+plt.imshow(o)
+plt.axis("off")
+
+plt.subplot(132)
+plt.imshow(e)
+plt.axis("off")
+
+plt.subplot(133)
+plt.imshow(b)
+plt.axis("off")
+
+plt.show()
